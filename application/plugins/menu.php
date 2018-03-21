ff77b260092f21c139cee6b0ef5e01823716004c
Zadnepr Andrey <zadnepr@gmail.com>
2018-03-21 18:18:39 +0300

    menu

--- application/plugins/menu.php
+++ application/plugins/menu.php
@@ -24,44 +24,46 @@ if(!class_exists('menu', false))
 			extract(shortcode_atts(array(
 				'url' => '',
 				'type' => '',
-				'project' => ''
+				'project' => '',
+				'menu_id' => '1',
 			), $atts));
 
 			$this->model_menu = new model_menu();
 			$this->model_menu_items = $this->model_menu->model_menu_items();
 
-			$header_menu = $this->model_menu_items->getItemsWhere("menu_id = 1 AND parent_id = 0 AND status = 1", "position");
+			$header_menu = $this->model_menu_items->getItemsWhere("menu_id = '{$menu_id}' AND parent_id = 0 AND status = 1", "position");
 			$menu_html = '';
 			$mob_menu_html = '';
 			foreach ($header_menu AS $menu)
 			{
-				//$sub_menu = $this->model_menu_items->getItemsWhere("menu_id = 1 AND parent_id = ".$menu['id'], "position");
+				$menu_link = str_replace('%project_controller%', $project, $menu['link']);
+				if(strpos(Registry::get('REQUEST_URI'), $menu_link)!==FALSE){
 				$link = explode('/', $url);
-				$parent_id = $this->model_menu_items->getItemWhere("menu_id = 1 AND link = '".$link[1]."' AND status = 1", "id");
-
+				$parent_id = $menu['id'];
 				if ($parent_id)
 				{
-					$sub_menu = $this->model_menu_items->getItemsWhere("menu_id = 1 AND parent_id = ".$parent_id['id']." AND status = 1", "position");
+					$sub_menu = $this->model_menu_items->getItemsWhere("menu_id = '{$menu_id}' AND parent_id = ".$parent_id." AND status = 1", "position");
 				}
-
+			
 				$sub_html = '';
 				$mob_sub_html = '';
-				
+
 				foreach ($sub_menu AS $s_menu)
 				{
-					$link = str_replace('%project_controller%', $project, $s_menu['link']);
+					$menu_link = str_replace('%project_controller%', $project, $s_menu['link']);
 					$sub_html .= '
-						<li class="ajax-anchor-in '.$s_menu['class'].'"><a href="'.$GLOBALS['CONFIG']['HTTP_HOST'].'/'.$link.'">'.$s_menu['name'].'</a></li>
+						<li class="ajax-anchor-in '.$s_menu['class'].'"><a href="'.$menu_link.'">'.$s_menu['name'].'</a></li>
 					';
 
 					$mob_sub_html .= '
-						<li><a href="'.$GLOBALS['CONFIG']['HTTP_HOST'].'/'.$s_menu['link'].'">'.$s_menu['name'].'</a><em><i></i></em></li>
+						<li><a href="'.$menu_link.'">'.$s_menu['name'].'</a><em><i></i></em></li>
 					';
 				}
-				$link = str_replace('%project_controller%', $project, $menu['link']);
+				}
+				//var_dump(Registry::get('REQUEST_URI'));				var_dump($menu_link); var_dump(strpos(Registry::get('REQUEST_URI'), $menu_link));	echo "------\r\n";
 				$menu_html .= '
-					<li class="ajax-anchor '.$menu['class'].' '.(strpos(Registry::get('REQUEST_URI'), $link)?"active":"").'">
-	                    <a href="'.$GLOBALS['CONFIG']['HTTP_HOST'].'/'.$link.'">'.$menu['name'].'</a>
+					<li class="ajax-anchor '.$menu['class'].' '.(strpos(Registry::get('REQUEST_URI'), $menu_link)!==FALSE?"active":"").'">
+	                    <a href="'.$menu_link.'">'.$menu['name'].'</a>
 	                    <ul class="menu-sub-list">
 	                        '.$sub_html.'
 						</ul>
@@ -73,7 +75,7 @@ if(!class_exists('menu', false))
 					
 					$mob_menu_html .= '
 						<li class="has-drop-mobile">
-		                    <a href="'.$GLOBALS['CONFIG']['HTTP_HOST'].'/'.$link.'">'.$menu['name'].'</a>
+		                    <a href="'.$menu_link.'">'.$menu['name'].'</a>
 		                    <ul>
 		                        '.$mob_sub_html.'
 		                    </ul>
@@ -84,7 +86,7 @@ if(!class_exists('menu', false))
 				{
 					$mob_menu_html .= '
 						<li>
-		                    <a href="'.$GLOBALS['CONFIG']['HTTP_HOST'].'/'.$link.'">'.$menu['name'].'</a>
+		                    <a href="'.$menu_link.'">'.$menu['name'].'</a>
 		                </li>
 					';
 				}
